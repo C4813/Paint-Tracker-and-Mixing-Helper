0ebeed07d76189b77d257d9bf6e9167792b87bbc
Cable <c4813eve@gmail.com>
2025-11-19 16:16:31 +0000

    Update template.php

--- paint-table-shortcode/template.php
+++ paint-table-shortcode/template.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Front-end table template for Paint Table Shortcodes.
+ * Frontend table template for Paint Table Shortcodes.
  *
  * Expects:
- *  - $pct_paints: array of paints
- *  - $pct_range_title: string (range name, e.g. "Vallejo Model Color")
+ * - $pct_paints      : array of [ 'name', 'number', 'hex', 'links' ]
+ * - $pct_range_title : string (range name, e.g. "Vallejo Model Color")
  */
 
 if ( ! isset( $pct_paints ) || ! is_array( $pct_paints ) || empty( $pct_paints ) ) {
@@ -12,68 +12,81 @@ if ( ! isset( $pct_paints ) || ! is_array( $pct_paints ) || empty( $pct_paints )
 }
 ?>
 
-<div class="pct-table-wrapper">
+<div class="pct-table-container">
     <?php if ( ! empty( $pct_range_title ) ) : ?>
-        <h2 class="pct-table-title"><?php echo esc_html( $pct_range_title ); ?></h2>
+        <div class="pct-range-title">
+            <?php echo esc_html( $pct_range_title ); ?>
+        </div>
     <?php endif; ?>
 
-    <table class="pct-table">
-        <thead>
-        <tr>
-            <th class="pct-col-swatch" scope="col">&nbsp;</th>
-            <th scope="col"><?php esc_html_e( 'Colour', 'pct' ); ?></th>
-            <th scope="col"><?php esc_html_e( 'Number', 'pct' ); ?></th>
-            <th scope="col"><?php esc_html_e( 'Models', 'pct' ); ?></th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ( $pct_paints as $paint ) : ?>
-            <?php
-            $name   = isset( $paint['name'] ) ? $paint['name'] : '';
-            $number = isset( $paint['number'] ) ? $paint['number'] : '';
-            $hex    = isset( $paint['hex'] ) ? $paint['hex'] : '';
-            $links  = isset( $paint['links'] ) && is_array( $paint['links'] ) ? $paint['links'] : [];
-            ?>
-            <tr>
-                <td class="pct-cell-swatch">
-                    <?php if ( $hex ) : ?>
-                        <span class="pct-swatch" style="background-color: <?php echo esc_attr( $hex ); ?>"></span>
-                    <?php endif; ?>
-                </td>
-                <td class="pct-cell-name">
-                    <span class="pct-name"><?php echo esc_html( $name ); ?></span>
-                </td>
-                <td class="pct-cell-number">
-                    <?php echo esc_html( $number ); ?>
-                </td>
-                <td class="pct-cell-links">
-                    <?php if ( ! empty( $links ) ) : ?>
-                        <?php foreach ( $links as $i => $link ) :
-                            $url    = isset( $link['url'] ) ? $link['url'] : '';
-                            $ltitle = isset( $link['title'] ) ? $link['title'] : '';
-                            if ( ! $url ) {
-                                continue;
-                            }
+    <div class="pct-table-wrapper">
+        <table class="pct-table">
+            <thead>
+                <tr>
+                    <th class="pct-swatch-header" aria-hidden="true"></th>
+                    <th><?php esc_html_e( 'Colour', 'pct' ); ?></th>
+                    <th><?php esc_html_e( 'Number', 'pct' ); ?></th>
+                    <th class="pct-models-header"><?php esc_html_e( 'Models', 'pct' ); ?></th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php foreach ( $pct_paints as $paint ) :
+                $name   = isset( $paint['name'] )   ? $paint['name']   : '';
+                $number = isset( $paint['number'] ) ? $paint['number'] : '';
+                $hex    = isset( $paint['hex'] )    ? $paint['hex']    : '';
+                $links  = isset( $paint['links'] )  && is_array( $paint['links'] )
+                    ? $paint['links']
+                    : [];
+                ?>
+                <tr>
+                    <td class="pct-swatch-cell">
+                        <?php if ( $hex ) : ?>
+                            <span class="pct-swatch" style="background-color: <?php echo esc_attr( $hex ); ?>"></span>
+                        <?php endif; ?>
+                    </td>
+                    <td class="pct-name-cell">
+                        <span class="pct-name"><?php echo esc_html( $name ); ?></span>
+                    </td>
+                    <td class="pct-number">
+                        <?php echo esc_html( $number ); ?>
+                    </td>
+                    <td class="pct-models">
+                        <?php if ( ! empty( $links ) ) : ?>
+                            <?php
+                            $total_links = count( $links );
+                            $shown       = 0;
 
-                            if ( '' === $ltitle ) {
-                                $ltitle = ( count( $links ) > 1 )
-                                    ? sprintf( __( 'View %d', 'pct' ), $i + 1 )
-                                    : __( 'View', 'pct' );
-                            }
-                            ?>
-                            <a href="<?php echo esc_url( $url ); ?>" target="_blank" rel="noopener">
-                                <?php echo esc_html( $ltitle ); ?>
-                            </a>
-                            <?php if ( $i < count( $links ) - 1 ) : ?>
-                                <br>
-                            <?php endif; ?>
-                        <?php endforeach; ?>
-                    <?php else : ?>
-                        &mdash;
-                    <?php endif; ?>
-                </td>
-            </tr>
-        <?php endforeach; ?>
-        </tbody>
-    </table>
+                            foreach ( $links as $i => $link ) {
+                                $url    = isset( $link['url'] )   ? $link['url']   : '';
+                                $ltitle = isset( $link['title'] ) ? $link['title'] : '';
+
+                                if ( ! $url ) {
+                                    continue;
+                                }
+
+                                // Fallback title if none set
+                                if ( '' === $ltitle ) {
+                                    $ltitle = ( $total_links > 1 )
+                                        ? sprintf( __( 'View %d', 'pct' ), $i + 1 )
+                                        : __( 'View', 'pct' );
+                                }
+
+                                $shown++;
+                                ?>
+                                <a href="<?php echo esc_url( $url ); ?>" target="_blank" rel="noopener">
+                                    <?php echo esc_html( $ltitle ); ?>
+                                </a>
+                                <?php if ( $shown < $total_links ) : ?>
+                                    <br>
+                                <?php endif; ?>
+                            <?php } ?>
+                        <?php else : ?>
+                            &mdash;
+                        <?php endif; ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
